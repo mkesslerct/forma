3728a3bb9a70737df6ea341384b26c01f09f7e38
DESKTOP-G26BG8S\Mathieu <mkesslerct@gmail.com>
2018-05-15 12:51:11 +0200

    Incluimos algo de purrr para probar importación de librería

--- R/functions.R
+++ R/functions.R
@@ -2,3 +2,23 @@
 genera_gauss <- function(n = 2){
   rnorm(n)
 }
+## prepara json_data
+#' Manipula json de eventos y estructura del curso
+#'
+#'@import purrr
+#'@import tidyr
+#'@import lubridate
+#'@import tibble
+#'@import dplyr
+#'
+#' @param json_data A string
+#'
+#' @return la longitud de la lista
+#' @export
+#'
+#' @examples
+#' preparar_json({{"a":3,"nombre":"Mathieu"} })
+preparar_json <- function(json_data){
+  #tt <- jsonlite::fromJSON(json_data)
+  map_int(list(u = 1:3, v = list(a = 1, b = 2)), ~ length(.x))
+}
